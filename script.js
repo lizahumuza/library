df4fe38e8f37218ea109663ed82e100844e3560d
Elizabeth Ahumuza <elizabeth@PNPs-MacBook-Pro.local>
2023-04-01 17:01:28 +0300

    Formated script.js

--- script.js
+++ script.js
@@ -1,78 +1,74 @@
-let myLibrary=[]
-  
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
-this.title = title;
-this.author = author;
-this.pages = pages;
-this.read = read;  
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-Book.prototype.info = function() {
-   return(`${this.title} by ${this.author}, ${this.pages} pages , <strong>${this.read ? "read" : "not read"}</strong>`)
-}
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages , <strong>${
+    this.read ? "read" : "not read"
+  }</strong>`;
+};
 
 Book.prototype.changeReadStatus = function () {
-this.read = !this.read;
+  this.read = !this.read;
+};
+
+function addBookToLibrary() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("status").checked;
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayLibrary();
+  document.getElementById("newForm").reset();
+  document.getElementById("newForm").style.display = "none";
 }
 
-  function addBookToLibrary() {
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("status").checked;
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    displayLibrary();
-    document.getElementById("newForm").reset();
-    document.getElementById("newForm").style.display = "none";
-  }
-
-  function displayLibrary() {
-    let libraryDiv = document.getElementById("library");
-    libraryDiv.textContent = "";
-    for (let i = 0; i < myLibrary.length; i++) {
-      let book = myLibrary[i];
-      let bookDiv = document.createElement("div");
-      bookDiv.innerHTML = `<div class=parent>
+function displayLibrary() {
+  const libraryDiv = document.getElementById("library");
+  libraryDiv.textContent = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    const bookDiv = document.createElement("div");
+    bookDiv.innerHTML = `<div class=parent>
                        <div class="books">${book.info()}</div>
                        <div>
                        <button data-index="${i}" class="delButton">Delete</button>
                        <button data-index="${i}" class="change-read-status-button">Read Status</button>
                        </div>
                        </div>`;
-                       
-      libraryDiv.appendChild(bookDiv);
-    }
+
+    libraryDiv.appendChild(bookDiv);
   }
+}
 
-let btn = document.getElementById("new-book-btn")
+const btn = document.getElementById("new-book-btn");
 btn.addEventListener("click", function () {
-      document.getElementById("newForm").style.display = "block";
-    });
+  document.getElementById("newForm").style.display = "block";
+});
 
-let form1 = document.getElementById("newForm")
+const form1 = document.getElementById("newForm");
 form1.addEventListener("submit", function (event) {
-      event.preventDefault(); 
-      addBookToLibrary();
-    }); 
+  event.preventDefault();
+  addBookToLibrary();
+});
 
-let lib= document.getElementById("library")  
+const lib = document.getElementById("library");
 lib.addEventListener("click", function (event) {
-      if (event.target.classList.contains("delButton")) {
-        let index = event.target.getAttribute("data-index");
-        myLibrary.splice(index, 1);
-        displayLibrary();
-      } else if (
-        event.target.classList.contains("change-read-status-button")
-      ) {
-        let index = event.target.getAttribute("data-index");
-        myLibrary[index].changeReadStatus();
-        displayLibrary();
-      }
-    });     
-    
-    
-//myLibrary.push(new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
-//myLibrary.push(new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, true));
+  if (event.target.classList.contains("delButton")) {
+    const index = event.target.getAttribute("data-index");
+    myLibrary.splice(index, 1);
+    displayLibrary();
+  } else if (event.target.classList.contains("change-read-status-button")) {
+    const index = event.target.getAttribute("data-index");
+    myLibrary[index].changeReadStatus();
+    displayLibrary();
+  }
+});
 
 displayLibrary();
